77f9e60f0a45d2a3d696475fc40d23105abef195
Caroline <carolinelappleby@gmail.com>
2018-07-31 16:25:04 +0100

    changed route for users

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
   onUsernameSubmitted(username) {
     console.log("NAME", username);
-    fetch("http://localhost:3001/users", {
+    fetch("https://schlack.herokuapp.com/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
